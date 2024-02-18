29686fc07da3ba3134850e101e2b9b348367aba0
UncleSamsun <alswns5620@naver.com>
2024-02-19 01:28:47 +0900

    9-10 study

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const API_KEY = ``
 let newsList = []
+const menus = document.querySelectorAll(".menus button")
+menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)))
 
 const getLatestNews = async () => {
     const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
@@ -8,6 +10,28 @@ const getLatestNews = async () => {
     newsList = data.articles
     render()
     console.log("newsList: ", newsList)
+}
+
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase()
+    console.log(category)
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=${category}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    newsList = data.articles
+    render()
+    console.log("Category newsList: ", newsList)
+}
+
+const getNewsByKeyword = async () => {
+    const keyword = document.getElementById("search-input").value
+    console.log(keyword)
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=${keyword}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    newsList = data.articles
+    render()
+    console.log("Category newsList: ", newsList)
 }
 
 const render = () => {
