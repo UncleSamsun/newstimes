9aade2ecf4617da2102ce5e78a9a400e8c7a8016
UncleSamsun <alswns5620@naver.com>
2024-02-19 00:15:44 +0900

    noona api apply

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const API_KEY = `bc0ec150736a41bbb9de6afd7e2d10d0`
+const API_KEY = ``
 let news = []
 
 const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
     const response = await fetch(url)
     const data = await response.json()
     news = data.articles
